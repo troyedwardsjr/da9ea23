cb8891d717b092143a9d1d1087521ac0529361ed
troyedwardsjr <troyedwardsjr@gmail.com>
2024-10-24 15:57:53 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use axum::{extract::Path, http::StatusCode, routing::{get, post}, Json, Router};
+use std::collections::HashMap;
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+
+#[derive(Eq, PartialEq, Serialize, Deserialize)]
+struct Movie {
+    id: String,
+    name: String,
+    year: u16,
+    was_good: bool,
+}
+
+type ID = String;
+// HashMaps mocking database
+static mut MOVIES: Lazy<HashMap<ID, Movie>> = Lazy::new(|| {
+    let mut m = HashMap::new();
+    m.insert("0".to_string(), Movie {
+        id: format!("{}", 0),
+        name: "Titanic".to_string(),
+        year: 1990,
+        was_good: false
+    });
+    m.insert("1".to_string(), Movie {
+        id: format!("{}", 1),
+        name: "Fight Club".to_string(),
+        year: 1999,
+        was_good: true
+    });
+    m
+});
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/movie/:id", get(get_movie))
+        .route("/movie", post(post_movie));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_movie(Path(id): Path<String>) -> Result<Json<Movie>, StatusCode> {
+    let movie = unsafe { MOVIES.get(&id) }
+        .ok_or_else(|| StatusCode::NOT_FOUND)?;
+    let movie: Json<Movie> = Json(Movie {
+        id: movie.id.clone(),
+        name: movie.name.clone(),
+        year: movie.year,
+        was_good: movie.was_good
+    });
+    Ok(movie)
+}
+
+async fn post_movie(Json(payload): Json<Movie>) -> Result<StatusCode, StatusCode> {
+    if payload.id.is_empty() {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+    // Insert the movie into the HashMap
+    unsafe {
+        MOVIES.insert(payload.id.clone(), Movie {
+            id: payload.id.clone(),
+            name: payload.name.clone(),
+            year: payload.year,
+            was_good: payload.was_good
+        });
+    }
+    Ok(StatusCode::ACCEPTED)
+}
